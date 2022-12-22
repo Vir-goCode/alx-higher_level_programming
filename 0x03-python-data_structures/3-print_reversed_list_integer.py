16e38c35235cb66d53c3c5c72280e0bdfa94161d
Vir-goCode <rdk365@outlook.com>
2022-12-22 23:36:20 +0000

    Data Structures: Lists, Tuples

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,4 +1,5 @@
-st_integer(my_list=[]):
+#!/usr/bin/python3
+def print_reversed_list_integer(my_list=[]):
     "prints all integers of a list, in reverse order"
     if my_list:
         for n in range(0, len(my_list)):
